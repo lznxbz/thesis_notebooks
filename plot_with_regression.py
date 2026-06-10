22beada83e4f5799ae3882b71b648cb2bee1e8bd
lznxbz <lznxbz@126.com>
2026-06-10 15:25:38 +0200

    3

--- plot_with_regression.py
+++ plot_with_regression.py
@@ -6,11 +6,14 @@ import numpy as np
 color_l = list(mcolors.BASE_COLORS.keys())
 color_index = 0
 
-def plot_with_regression(grid, plot_n, x_vals, y_vals, label):
+def plot_with_regression(grid, plot_n, x_vals, y_vals, label, reginleg = False):
     global color_index
     # Linear Regression
     slope, intercept, r, p, se = linregress(x_vals, y_vals)
     y_lr = slope * x_vals + intercept
+
+    if reginleg:
+        label = f"{label}  {slope:.2f}  {intercept:.2f}  {r**2:.2f}"
     # Plot
     grid.plot(x_vals, y_lr, "-", color=color_l[plot_n])
     grid.plot(x_vals, y_vals, "o", label=label, color=color_l[plot_n])
